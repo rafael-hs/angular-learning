2970e839bcb73b676951acd1fe1507698b5899c1
Rafael Honório da Silva <rafael.honorio@NET.SEFAZ.ES.GOV.BR>
2019-09-23 11:49:29 -0300

    initial commit

---
+++ typescript/app.ts
@@ -0,0 +1,7 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message)
+
+let episode: number = 4
+console.log(`This is episode ${4}`)
+episode =  episode + 1
+console.log(`Next episode is ${episode}`)
